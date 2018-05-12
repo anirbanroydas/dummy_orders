8aa1582f697bec5885a360f84e582a83f6d4570d
Anirban Roy Das <anirban.nick@gmail.com>
2018-05-12 20:09:49 +0530

    Add configuration and setup for making the system Architecture 3 compatible

--- orders/server.py
+++ orders/server.py
@@ -7,9 +7,12 @@ from orders.log import getCustomLogger
 from orders.routes import addRoutes
 from orders.usecases.transact import TransactionProcessor, TransactionValidator
 from orders.usecases.fraudcheck import ExternalFraudChecker, InProcessFraudChecker
-from orders.usecases.alert import ExternalAlertSender, InProcessAlertSender
-from orders.gateway import HTTPTransportGateway
-from orders.db import MongoDBClient
+from orders.usecases.alert import (
+    ExternalServiceAlertSender, InProcessAlertSender, MessageBrokerAlertSender
+)
+from orders.gateway import HTTPTransportGateway, RabbitMqTransportGateway
+from orders.mongodb_client import DummyMongoDBClient
+from orders.rabbitmq_client import AioPikaClient
 
 app = Sanic('orders', configure_logging=True)
 
@@ -18,7 +21,7 @@ log = getCustomLogger(__name__)
 
 async def setupDB(host, port, name, user, password):
     log.info("Setting up DB")
-    db = MongoDBClient(host, port, name, user, password)
+    db = DummyMongoDBClient(host, port, name, user, password)
     await db.setup()
     return db
 
@@ -30,11 +33,23 @@ def getTransactionInteractor(app):
 
     # instantiate the type of transport gateway that would be used to
     # send request/communicate with external alert service, external fraud checking service, etc.
-    alertSenderGateway = HTTPTransportGateway(
-        client=app.HTTPClient,
-        host=app.config.ALERT_SENDER_SERVICE_HOST,
-        uri=app.config.ALERT_SENDER_SERVICE_URI,
-        method='POST'
+    # alertSenderGateway = HTTPTransportGateway(
+    #     client=app.HTTPClient,
+    #     host=app.config.ALERT_SENDER_SERVICE_HOST,
+    #     uri=app.config.ALERT_SENDER_SERVICE_URI,
+    #     method='POST'
+    # )
+    alertSenderGateway = RabbitMqTransportGateway(
+        rabbitMqClient=app.MessageBrokerClient,
+        exchange='dummy-exchange',
+        routing_key='dummy-alerts',
+        options={
+            'set_qos': 1,
+            'exchangeType': 'topic',
+            'queueDurable': True,
+            'bindingKey': 'dummy-alerts',
+            'deliverMode': 'persistent'
+        }
     )
     fraudCheckerGateway = HTTPTransportGateway(
         client=app.HTTPClient,
@@ -42,12 +57,15 @@ def getTransactionInteractor(app):
         uri=app.config.FRAUD_CHECKER_SERVICE_URI,
         method='POST'
     )
+
     # instantiate the alert sender
     alertSender = InProcessAlertSender()
-    #alertSender = ExternalAlertSender(gateway=alertSenderGateway)
+    #alertSender = ExternalServiceAlertSender(gateway=alertSenderGateway)
+    alertSender = MessageBrokerAlertSender(messageGateway=alertSenderGateway)
+
     # instantiate the fraud checker (could also have been the InProcessFraudChecker )
-    fraudChecker = InProcessFraudChecker()
-    #fraudChecker = ExternalFraudChecker(gateway=fraudCheckerGateway)
+    # fraudChecker = InProcessFraudChecker()
+    fraudChecker = ExternalFraudChecker(gateway=fraudCheckerGateway)
     # create the transaction interactor
     return TransactionProcessor(
         transactionRepo=app.DB,
@@ -59,20 +77,39 @@ def getTransactionInteractor(app):
 
 def setupAiohttpClientSession(loop):
     return ClientSession(loop=loop)
+
+async def setupMessageBroker(app, loop):
+    client = AioPikaClient(
+        username=app.config.MESSAGE_BROKER_SERVICE_USERNAME,
+        password=app.config.MESSAGE_BROKER_SERVICE_PASSWORD,
+        host=app.config.MESSAGE_BROKER_SERVICE_HOST,
+        port=int(app.config.MESSAGE_BROKER_SERVICE_PORT),
+        virtualhoat=app.config.MESSAGE_BROKER_SERVICE_VIRTUALHOST,
+        loop=loop
+    )
     
+    # setup the connection and channel that will be used across the app
+    log.info("Setting up Message Broker...")
+    await client.setup()
+    return client
 
 @app.listener('before_server_start')
 async def before_start(app, loop):
+    # add the api routes
+    addRoutes(app)
     # first add async http client to the app using aiohttp
     app.HTTPClient = setupAiohttpClientSession(loop)
     # now setup the DB connections
-    app.DB = await setupDB(app.config.DB_HOST, app.config.DB_PORT,
-        app.config.DB_NAME, app.config.DB_USER, app.config.DB_PASSWORD)
-    # add the api routes
-    addRoutes(app)
+    app.DB = await setupDB(
+        app.config.DB_HOST, int(app.config.DB_PORT), app.config.DB_NAME,
+        app.config.DB_USER, app.config.DB_PASSWORD
+    )
+    # get the message broker connection
+    app.MessageBrokerClient = await setupMessageBroker(app, loop)
     # get the usecase interactors here, so that app can use the interactors
     # to perform the usecases, here get/create the transaction specific interactor
     app.TransInteractor = getTransactionInteractor(app)
+    
     log.info('Starting server on http://{}:{}'.format(app.config.HOST, app.config.PORT))
 
 
@@ -88,12 +125,17 @@ async def before_stop(app, loop):
 
 @app.listener('after_server_stop')
 async def after_stop(app, loop):
+    # close the db connection
+    log.info("Closing Db connection...")
     await app.DB.close()
+    # close the message broker connection
+    log.info("Closing message broker connection...")
+    await app.MessageBrokerClient.close()
 
 
 def startServer():
-    app.config.from_envvar('SANIC_APP_ORDERS_SETTINGS')
-    app.run(host=app.config.HOST, port=app.config.PORT, workers=app.config.WORKERS)
+    # app.config.from_envvar('SANIC_APP_ORDERS_SETTINGS')
+    app.run(host=app.config.HOST, port=int(app.config.PORT), workers=int(app.config.WORKERS))
         
 
 if __name__ == "__main__":
